ebb3bbe6a12108906aae8f92c0b87108d3e9a44f
AmirMahdiSedghi <asedghi1380@gmail.com>
2021-06-11 16:03:13 +0430

    Color enum & Player,Piece Classes added to project every player has a point.

---
+++ src/model/Player.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Player {
+    // every player has some pieces. it will be add here
+
+    private int point;
+
+    public int getPoint() {
+        return point;
+    }
+}
