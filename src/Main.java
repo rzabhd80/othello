ef59cbcc5505617d3fb20d1fed64d6dd56eaada2
reza <rzxsism80@gmail.com>
2021-06-11 12:18:35 +0430

    head to design

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
