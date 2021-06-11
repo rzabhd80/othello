a1ab26436366b92ac3a16b059edbd10ac26b0a78
reza <rzxsism80@gmail.com>
2021-06-12 01:17:09 +0430

    design completed tnx to amir

--- src/Main.java
+++ src/Main.java
@@ -13,10 +13,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("view/board.fxml"));
         fxmlLoader.load();
-<<<<<<< HEAD
-=======
-//        primaryStage.setFullScreen(true);
->>>>>>> 07bd01ebdecf7273d9f5762965e369de7832d458
         primaryStage.setScene(new Scene(fxmlLoader.getRoot()));
         primaryStage.show();
     }
