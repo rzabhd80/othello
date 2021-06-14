7a6569c9105ffbc1d0b0836820a5e0bae969f5d1
reza <rzxsism80@gmail.com>
2021-06-14 23:19:26 +0430

    checking the right of black piece done

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.Color;
 import model.Piece;
+import model.Player;
 import model.Status;
 
 import java.net.URL;
@@ -29,12 +30,130 @@ public class BoardController implements Initializable {
     @FXML
     private Label playerName2;
 
+    /**
+     * this global array is meant to store all the pieces of the field
+     *
+     * @author rezaBh
+     */
     private final Piece[][] pieces = new Piece[8][8];
 
+    /**
+     * this method is used for maintaining the possible selectable  pieces
+     * we gotta check the row,left and right of the piece
+     * it makes the pieces selectable or not selectable
+     * if it returns true, it means that given piece is selectable
+     *
+     * @param piece
+     * @author rezaBH
+     */
+
+    private void checkingTheRightRowForBlack(Piece piece, int i, int j) {
+        System.out.println(i + "" + j);
+        if (piece.getPieceColor() != null) {
+            if (piece.getPieceColor().equals(Color.green)) {
+                //if the next piece has diff color or its null, it doesnt need
+                if (pieces[i][j + 1].getStatus().equals(Status.selected)) {
+                    if (!pieces[i][j + 1].getPieceColor().equals(piece.getPieceColor()) && pieces[i][j + 1].getStatus().
+                            equals(Status.selected)) {
+                        //checks all next buttons to see if any of them is white
+                        for (int k = j; k < 8; k++) {
+                            if (foundWhitePiece(pieces[i][k])) {
+                                // checking if there is a empty cell after white buttons
+                                for (int p = k + 1; p < 8; p++) {
+                                    if (pieces[i][p].getStatus().equals(Status.unselectable)) {
+                                        pieces[i][p].setPieceSelectable();
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        //then we need to check previous pieces
+                        return;
+                    }
+                }
+            } else {
+                return;
+            }
+        }
+    }
+
+    private void checkingTheRightRowForWhite(Piece piece, int i, int j) {
+        if (piece.getPieceColor().equals(Color.white)) {
+            //if the next or previous piece has diff color return false
+            if (pieces[i][j + 1].getPieceColor() != null) {
+                if (!pieces[i][j + 1].getPieceColor().equals(piece.getPieceColor()) && pieces[i][j + 1].getPieceColor() != null) {
+                    //checks all next buttons to see if any of them is white
+                    for (int k = i; k < 8; k++) {
+                        if (foundGreenPiece(pieces[k][j])) {
+                            // checking if there is a empty cell after white buttons
+                            for (int p = k; p < 8; p++) {
+                                if (pieces[p][j].getStatus().equals(Status.unselectable)) {
+                                    pieces[p][j].setStatus(Status.selectable);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * this methods are meant to check if the given piece has the color of green
+     *
+     * @param piece
+     * @return
+     * @author reza bh
+     */
+    public boolean foundGreenPiece(Piece piece) {
+        if (piece.getPieceColor().equals(Color.green))
+            return true;
+        return false;
+    }
+
+    public boolean foundWhitePiece(Piece piece) {
+        if (piece.getPieceColor().equals(Color.white))
+            return true;
+        return false;
+    }
+
+    /**
+     * flipper method is used for flipping the coins
+     *
+     * @author reza BH
+     */
+    public void flipper() {
+
+    }
+
+    /**
+     * setting the selectables of table
+     *
+     * @author reza Bh
+     */
+    public void setSelectables() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                checkingTheRightRowForBlack(pieces[i][j], i, j);
+                //BUG HERE
+                //checkingTheRightRowForWhite(pieces[i][j], i, j);
+            }
+
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        Player player1 = new Player("reza");
+        Player player2 = new Player("amir");
+        playerName1.setText(player1.getName());
+        playerName2.setText(player2.getName());
         initPieces();
+        setSelectables();
+
     }
 
 
@@ -67,5 +186,4 @@ public class BoardController implements Initializable {
             board.getChildren().add(hBox);
         }
     }
-
 }
